eaf8c98a6f108419d347a0b1380917842a3a1a1f
Charles Yeo <yeocheahhaw92@gmail.com>
2020-03-11 00:30:19 +0800

    Setup ant design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import NextApp from 'next/app';
 import React from 'react';
 
+import 'antd/dist/antd.css';
+
 export default class extends NextApp {
   render() {
     const { Component, pageProps } = this.props;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from 'antd';
 
 const App = () => {
-  return <div>What to eat</div>;
+  return (
+    <div>
+      What to eat<Button type="primary">Asd</Button>
+    </div>
+  );
 };
 
 export default App;
